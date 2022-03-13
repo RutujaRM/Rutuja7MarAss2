ef9ac1b5ebc36e0a0db310b4eb6274221f8a8076
RutujaRM <96306705+RutujaRM@users.noreply.github.com>
2022-03-12 22:24:16 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+function home(div_id){
+     var div1= document.getElementById("form1div3");
+     var div2= document.getElementById("form1div4");
+
+     var div3= document.getElementById("container1");
+     
+     if(div_id=="1")
+     {
+        div1.style.display = "initial";
+     }
+     if(div_id=="01")
+     {
+        div2.style.display = "initial";
+     }
+     if(div_id=="2")
+     {
+        div3.style.display = "initial";
+     }
+     if(div_id=="3")
+     {
+        alert("30 Follow Request")
+     }
+}
+
+
+
